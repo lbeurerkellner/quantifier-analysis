222e2b7c88dfe6075c6f74d935954c54b8d7a6c7
Luca Beurer-Kellner <lucabeurerkellner@gmail.com>
2020-01-22 22:52:21 +0100

    equivalence-based merging of bindings

--- src/instantiation-graph/e-matching.ts
+++ src/instantiation-graph/e-matching.ts
@@ -8,14 +8,16 @@ import { FunctionApplicationExpr, Constant, NodeType } from './../ast/parser';
 export function match(pattern : FunctionApplicationExpr|Constant, term : TermNode, argumentIdx : number = 0) : Map<string, TermNode>[] {
     const functionName = pattern.name;
     
-    // construct set of matching candidates
+    // construct set of matching candidates (terms equivalent to term)
     const candidates = new Set(term.equivalenceClass) 
     candidates.add(term);
 
     if (pattern.type === NodeType.CONSTANT && argumentIdx !== 0) {
-        throw new Error("Invalid state: Cannot match against a specified argument with pattern being a variable.");
+        throw new Error("Invalid State: Cannot match against a specified argument with pattern being a variable.");
     }
 
+    // in case the argumentIdx exceeds the currently matched FunctionApplicationExpr-based
+    // pattern, an empty binding ends the recursive calling of this function for each function argument
     if ((pattern.type === NodeType.FUNC_APPLICATION) && 
         (pattern as FunctionApplicationExpr).args.length <= argumentIdx) {
         
@@ -60,7 +62,12 @@ export function mergeBindings(multipleBindings : Map<string, TermNode>[]) : Map<
     for (let bindings of multipleBindings) {
         for (let b of Array.from(bindings.entries())) {
             if (mergedBindings.has(b[0])) {
-                if (mergedBindings.get(b[0]) !== b[1]) {
+                if (mergedBindings.get(b[0]) === b[1]) {
+                    mergedBindings.set(b[0], b[1]);
+                } else if (mergedBindings.get(b[0])?.equivalenceClass.has(b[1])) {
+                    // nothing to do since an equivalent binding has already
+                    // been added to mergedBindings
+                } else {
                     return null; // bindings are incompatible
                 }
             } else {
